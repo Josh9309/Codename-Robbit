4c12fb21028c9d4e048b686a5a2e65f1f171de32
Josh Malmquist <joshmalmquist96@gmail.com>
2018-07-25 20:04:08 -0400

    Created Robbit Player Blueprint + C++ Class. Imported Robbit Test model
    -Robbit Test Model with test animations imported into the engine
    -Began experimenting with how to progam robbit's player movement. 
    -Created blueprint and c++ class for robbit pawn
    
    Notes: So right now I am experimenting with how to properly move robbit around. Right now I am programming the movement through blueprints. But I am running into the issue where robbit is not responding to collisions and is moving through objects. I suspect this is because I am hard updating the actor position. I am researching how to properly move robbit while keeping the physics working.

---
+++ Source/Codename_Robbit/RobbitMovementComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "RobbitMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CODENAME_ROBBIT_API URobbitMovementComponent : public UPawnMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
